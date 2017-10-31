0ca4a77dd3ca1d3df858a00708e99628db538380
Maksim Galiev <v-makgal@microsoft.com>
2017-10-31 17:28:13 +0300

    added UT for capabilities

--- test/visualTest.ts
+++ test/visualTest.ts
@@ -294,5 +294,33 @@ module powerbi.extensibility.visual.test {
                 }, DefaultTimeout);
             });
         });
+
+        describe("Capabilities tests", () => {
+            it("all items having displayName should have displayNameKey property", () => {
+                jasmine.getJSONFixtures().fixturesPath = "base";
+
+                let jsonData = getJSONFixture("capabilities.json");
+
+                let objectsChecker: Function = (obj) => {
+                    for (let property in obj) {
+                        let value: any = obj[property];
+
+                        if (property === "enumeration") {
+                            continue;
+                        }
+
+                        if (value.displayName) {
+                            expect(value.displayNameKey).toBeDefined();
+                        }
+
+                        if (typeof value === "object") {
+                            objectsChecker(value);
+                        }
+                    }
+                };
+
+                objectsChecker(jsonData);
+            });
+        });
     });
 }
